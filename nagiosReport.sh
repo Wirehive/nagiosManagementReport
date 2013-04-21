b5aa535a89afd38601ad74e0b0178e4ffd463b59
Simon Green <simonjgreen@me.com>
2013-04-21 14:32:16 +0100

    Initial commit

---
+++ nagiosReport.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+FROM="nagios@company.com"
+DESTINATIONS="salesteam.company.com"
+
+NAGIOSCGI="http://monitoring.company.com/nagios/cgi-bin/summary.cgi?report=1&displaytype=3&alerttypes=3&statetypes=2&hoststates=1&servicestates=56&limit=1000&noheader&timeperiod="
+
+USERNAME="nagiosuser"
+PASSWORD="nagiospass"
+
+DAILYPERIOD="last24hours"
+WEEKLYPERIOD="last7days"
+MONTHLYPERIOD="last31days"
+
+#Used to make links in the email work
+FIXUPREGEX="s/extinfo.cgi/http:\/\/monitoring.company.com\/nagios\/cgi-bin\/extinfo.cgi/g"
+
+#Used to make HTML email render correctly
+CONTENTTYPE="Content-Type: text/html"
+MIMEVERSION="MIME-Version: 1.0"
+
+
+#DAILY - Always runs
+curl --silent --user $USERNAME:$PASSWORD --url "$NAGIOSCGI$DAILYPERIOD" \
+| sed -e "$FIXUPREGEX" \
+| mail -a "From:$FROM" -a "$CONTENTTYPE" -a "$MIMEVERSION" -s "Alerts Report: `date "+%A %b %d %Y" --date=yesterday`" $DESTINATIONS
+
+#WEEKLY - Only runs on Monday
+if [[ $(date +%u) == 1 ]]; then
+        curl --silent --user $USERNAME:$PASSWORD --url "$NAGIOSCGI$WEEKLYPERIOD" \
+        | sed -e "$FIXUPREGEX" \
+        | mail -a "From:$FROM" -a "$CONTENTTYPE" -a "$MIMEVERSION" -s "Alerts Report: Week `date +%V --date=yesterday`" $DESTINATIONS
+fi
+
+#MONTHLY - Only runs on 1st of month
+if [[ $(date +%d) == 1 ]]; then
+        curl --silent --user $USERNAME:$PASSWORD --url "$NAGIOSCGI$MONTHLYPERIOD" \
+        | sed -e "$FIXUPREGEX" \
+        | mail -a "From:$FROM" -a "$CONTENTTYPE" -a "$MIMEVERSION" -s "Alerts Report: `date "+%b %Y" --date=yesterday`" $DESTINATIONS
+fi
